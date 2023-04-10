3defcf1d7eb577aeb262f351cbcc496231b8924f
Sk Soyeb Akhter <sksoyebakhter2003@gmail.com>
2023-04-10 13:45:21 +0530

    Added google sign in with nextauth package

---
+++ components/LoadingSpin.tsx
@@ -0,0 +1,10 @@
+
+function LoadingSpin() {
+  return (
+    <div className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full" role="status" aria-label="loading">
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+}
+
+export default LoadingSpin
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { signIn, useSession } from 'next-auth/react'
+import LoadingSpin from '@/components/LoadingSpin'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session, status } = useSession()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -11,22 +15,43 @@ export default function Home() {
           The best newsletter tool for&nbsp;
           <code className="font-mono font-bold">creators.</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="flex place-items-center gap-2 p-8 lg:p-0"
-            href="/api/signup"
-          >
-            Login with{' '}
+        {status === "loading" ? (
+          <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none'>
+            <LoadingSpin />
+          </div>
+        ) : status === "authenticated" && session && session.user && session.user.image ? (
+          <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none'>
             <Image
-              src="/google.svg"
-              alt="Google Logo"
-              className=""
-              width={16}
-              height={16}
-              priority
+              className='w-10 h-10 rounded-full'
+              src={session.user.image}
+              alt={"Profile picture missing"}
+              width={100}
+              height={100}
             />
-          </a>
-        </div>
+          </div>
+        ) : (
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+              className="flex place-items-center gap-2 p-8 lg:p-0"
+              href="/api/signup"
+              onClick={e => {
+                e.preventDefault()
+                signIn()
+              }}
+            >
+              Login with{' '}
+              <Image
+                src="/google.svg"
+                alt="Google Logo"
+                className=""
+                width={16}
+                height={16}
+                priority
+              />
+            </a>
+          </div>
+        )}
+
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
